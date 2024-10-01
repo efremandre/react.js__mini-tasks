6c241ad26a9e8724b66390bb2d67601eb4861be9
Andrey Eferemov <samo.dur.andre@gmail.com>
2024-10-01 17:39:49 +0300

    start task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
-import ListUsers from "./components/list-usres/ListUsers.tsx";
-import MainTitle from "./UI/title/Title.tsx";
 
 function App() {
 
     return (
         <div className='wrapper'>
-            <MainTitle>Список пользователей</MainTitle>
-            <ListUsers/>
+
         </div>
     )
 }
